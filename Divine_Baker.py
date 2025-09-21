e32723bb80d5403af9df6054fe92eb48d4ee3a9e
kaiser1x1 <thegamingraptor055@gmail.com>
2025-09-21 16:35:24 -0400

    added random bread name func

--- Divine_Baker.py
+++ Divine_Baker.py
@@ -4,16 +4,10 @@ import discord
 import logging
 from discord.ext import commands
 from openpyxl import load_workbook
-import csv
 
-# Example 1: Reading a CSV file and iterating through rows as lists
-with open('Bread_List.csv', 'r') as file:
-    reader = csv.reader(file)
-    try:
-        for row in reader:
-            print(row[0])
-    except:
-            print("ERROR")
+workbook = load_workbook('Bread List.xlsx')
+sheet = workbook.active
+breadTypes = []
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -26,15 +20,34 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 client = discord.Client(intents=intents)
 
+def get_rand_breadname():
+    pass
+
+def contains_elem(list, elem):
+    return (elem in list)
+
 @client.event
 async def on_ready():
+    # Iterate through columns from 'A' to 'C' (min_col=1, max_col=3)
+    # You can also specify min_row and max_row for a specific range within the columns
+    for col_cells in sheet.iter_cols(min_col=0, max_col=1, values_only=True):
+        for cell_value in col_cells:
+            # print(cell_value)
+            breadTypes.append(cell_value)
+
     print(f'We have logged in as {client.user}')
     for guild in client.guilds:
+        inUseNames = []
+        availableNames = []
         print(f"Guild: {guild.name} (ID: {guild.id})")
         print("Members:")
         for member in guild.members:
-            print(member.name, client.user.name)
-            if member.name != client.user.name:
+            # print(member.name, client.user.name)
+            # add current nicknames
+            if member.nick != None:
+                inUseNames.append(member.nick)
+            
+            if member.nick == None:
                 await setnick(commands.context, member, "okayLilBro")
             # print(f"  - {member.name} (ID: {member.id})")
 
@@ -61,4 +74,4 @@ async def setnick(ctx, member: discord.Member, new_nickname: str):
         # await ctx.send(f"An error occurred while changing the nickname: {e}")
         return
 
-client.run('sss', log_handler=handler)
+client.run('xxx', log_handler=handler)
